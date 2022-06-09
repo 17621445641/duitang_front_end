5b87a8d4a3edb7a3bb37eba7757a9e4a90ebad4f
kxp <2480274726@qq.com>
2022-06-09 18:22:52 +0800

    enter

--- src/components/Self_message.vue
+++ src/components/Self_message.vue
@@ -1,36 +1,73 @@
 <template>
 <div id="self_message">
-    <div class="self_list"><span>昵称：</span><span>{{message_list.user_name}}</span></div>
-    <div class="self_list"><span>性别：</span><span>{{message_list.sex}}</span>
-    <!-- <input type="radio" name="sex" value="boy" /><span class="sex_option">男</span>
-    <input type="radio" name="sex" value="gril" /><span class="sex_option">女</span>
-    <input type="radio" name="sex" value="secret" /><span class="sex_option">保密</span> -->
-    </div>
-    <div class="self_list"><span>生日：</span><span>{{message_list.birthday}}</span></div>
-    <div class="self_list"><span>所在地：</span>
-    <el-input style="width: 400px;"
+    <div class="self_list"><span class="attr">昵称：</span>
+    
+
+<el-input style="width: 220px;"
     type="text"
     placeholder="请输入内容"
     v-model="text"
     maxlength="10"
     show-word-limit
+    :rows="1"
     >
-</el-input><span>{{message_list.province}}</span></div>
-    <div class="self_list"><span>兴趣爱好：</span>
+    </el-input>
+
+
+
+    
+    <span>{{message_list.user_name}}</span></div>
+    <div class="self_list"><span class="attr">性别：</span>
+    <el-radio-group v-model="radio" >
+    <el-radio :label="3">男</el-radio>
+    <el-radio :label="6">女</el-radio>
+    <el-radio :label="9">保密</el-radio>
+    </el-radio-group>
+    <!-- <span>{{message_list.sex}}</span> -->
+    <!-- <input type="radio" name="sex" value="boy" /><span class="sex_option">男</span>
+    <input type="radio" name="sex" value="gril" /><span class="sex_option">女</span>
+    <input type="radio" name="sex" value="secret" /><span class="sex_option">保密</span> -->
+    </div>
+    <div class="self_list"><span class="attr">生日：</span>
+    <div class="block" style="display: inline-block">
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      @input="$forceUpdate()"
+      placeholder="选择日期">
+    </el-date-picker>
+  </div>
+  <!-- <span>{{message_list.birthday}}</span> -->
+  </div>
+    <div class="self_list"><span class="attr">所在地：</span>
+    <div class="block" style="display: inline-block">
+  <el-cascader
+    v-model="value"
+    placeholder="试试搜索：城市"
+    :options="options"
+    :clearable="true"
+    filterable
+    >
+    </el-cascader>
+    
+    </div>
+<!-- <span>{{message_list.province}}</span> -->
+</div>
+    <div class="self_list"><span class="attr">兴趣爱好：</span>
     <el-input style="width: 400px;"
     type="textarea"
     placeholder="请输入内容"
-    v-model="textarea"
+    v-model="textarea1"
     maxlength="20"
     show-word-limit
     :rows="1"
     >
     </el-input><span>{{message_list.hobby}}</span></div>
-    <div class="self_list"><span>个人简介：</span>
+    <div class="self_list"><span class="attr">个人简介：</span>
     <el-input style="width: 400px;"
     type="textarea"
     placeholder="请输入内容"
-    v-model="textarea"
+    v-model="textarea2"
     maxlength="100"
     show-word-limit
     :rows="5"
@@ -43,13 +80,18 @@
 
 <script>
 import axios from 'axios'
+import testImportJson from '../../static/中华人民共和国.json'
 // import bootstrap from '../css/bootstrap-4.6.1-dist/js/bootstrap'
 export default {
   data(){
     return{
       message_list:{},
-      textarea: "",
+      textarea1: "",
+      textarea2: "",
       text: '',
+      options:testImportJson,
+      radio: 3,
+      value1:"",
     }
     
   },
@@ -63,6 +105,21 @@ export default {
         .then(resp => {
             var that=this;
             that.message_list=resp.data.data;
+            if(that.message_list.user_name!=null || that.message_list.user_name!=""){
+                that.text=that.message_list.user_name
+            }
+            if(that.message_list.sex=='男' ){
+                that.radio=3
+            }
+            else if(that.message_list.sex=='女'){
+                that.radio=6
+            }
+            else{
+                that.radio=9
+            }
+            if(that.message_list.birthday!=null || that.message_list.user_name!=""){
+                that.value1=that.message_list.birthday
+            }
             console.log(that.message_list);
         }).catch(err => { //
             console.log('请求失败：'+ err.code + ',' + err.message);
@@ -85,7 +142,9 @@ export default {
         .catch(err => {
           console.log(err)
         })
-    }
+    },
+
+
   }
   
 }
